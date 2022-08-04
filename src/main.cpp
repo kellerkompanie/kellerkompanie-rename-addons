7f242935270d9d8d0411b1d5a109034028e55369
Schwaggot <tom.ryan@posteo.de>
2022-08-04 21:14:20 +0200

    Added verbose output

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,19 @@ int main(int argc, char *argv[]) {
                 // check addons
                 string absolutePath = filesystem::absolute(dir_entry.path()).string();
                 string lowerAbsPath = absolutePath.substr(0, absolutePath.length() - 6) + "addons";
+                cout << "renaming:" << endl;
+                cout << "\t" << absolutePath << endl;
+                cout << "\t" << lowerAbsPath << endl;
+                cout << endl;
                 filesystem::rename(absolutePath, lowerAbsPath);
             } else if (dir_entry.is_regular_file() && dir_entry.path().extension().string() == ".pbo" && filename != lowerFilename) {
                 // check lowercase pbo
                 string absolutePath = filesystem::absolute(dir_entry.path()).string();
                 string lowerAbsPath = absolutePath.substr(0, absolutePath.length() - filename.length()) + lowerFilename;
+                cout << "renaming:" << endl;
+                cout << "\t" << absolutePath << endl;
+                cout << "\t" << lowerAbsPath << endl;
+                cout << endl;
                 filesystem::rename(absolutePath, lowerAbsPath);
             }
         }
